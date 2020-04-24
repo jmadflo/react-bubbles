aeef92d925ef701c01ed39979e5e2daad574eb71
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-04-24 11:26:04 -0700

    did some styling on the login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Loader from 'react-loader-spinner'
 import { axiosWithAuth } from '../utils/axiosWithAuth'
 
+
 const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
@@ -48,34 +49,83 @@ const Login = props => {
   }
 
   return (
-    <>
+    <div style={pageStyles}>
       <h1>Welcome to the Bubble App!</h1>
-      <form>
-        <label htmlFor='username'>Username</label>
-        <input
-            id='username'
-            name='username'
-            type='text'
-            onChange={updateForm}
-        />
-        <label htmlFor='password'>Password</label>
-        <input
-            id='password'
-            name='password'
-            type='password'
-            onChange={updateForm}
-        />
-        <div className='formButtons'><button onClick={handleSubmit}>Log In</button></div>
+      <form style={formStyles}>
+        <label style={labelStyles} htmlFor='username'>Username  
+          <input
+              id='username'
+              name='username'
+              type='text'
+              onChange={updateForm}
+          />
+        </label>
+        <label style={labelStyles} htmlFor='password'>Password  
+          <input
+              id='password'
+              name='password'
+              type='password'
+              onChange={updateForm}
+          />
+        </label>
+        <button style={buttonStyles} onClick={handleSubmit}>Log In</button>
       </form>
       
-      {/* Render loader under form when loading */}
+      {/* Render loader when loading */}
       {isLoading && (
           <div>
               <Loader type='TailSpin' color='blue' width='50'/>
           </div>
       )}
-    </>
+    </div>
   )
+}
+
+// doing my login styles like this to avoid messing with scss file 
+
+const pageStyles = {
+  display: 'flex', 
+  flexDirection: 'column', 
+  alignItems: 'center', 
+  marginLeft: '25%'
+}
+
+const formStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-end',
+  width: '250px',
+  padding: '20px',
+  border: '2px solid gray',
+  borderRadius: '10px',
+  backgroundColor: 'black',
+}
+
+const labelStyles = {
+  margin: '0 0 4px 10px',
+  color: 'white',
+}
+
+const inputStyles = {
+  marginBottom: '20px',
+  width: '100%',
+  border: '1px solid gray',
+  borderRadius: '4px',
+  height: '20px',
+  marginLeft: '10px'
+}
+
+const buttonStyles = {
+  backgroundColor: '#2D88FF',
+  color: 'white',
+  width: '80px',
+  height: '25px',
+  fontSize: '0.8rem',
+  border: '2px solid white',
+  borderRadius: '5px',
+  cursor: 'pointer',
+  marginTop: '10px',
+  marginRight: '75px'
 }
 
 export default Login
