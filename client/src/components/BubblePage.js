49d43a3efd057b5550070707d9c5888cfeedc0dd
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-04-24 09:01:06 -0700

    successfully got colorlist from server

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,20 +1,30 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-import Bubbles from "./Bubbles";
-import ColorList from "./ColorList";
+import Bubbles from './Bubbles'
+import ColorList from './ColorList'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
 
 const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
+  const [colorList, setColorList] = useState([])
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
-
+  useEffect(() => {
+    axiosWithAuth().get('/api/colors')
+    .then(response => {
+      console.log(response)
+      setColorList(response.data)
+    })
+    .catch(error => {
+      alert('Could not get list of colors from server.', error )
+    })
+  }, []) // Since we did not add a dependency, the get request will only run once just after the component first renders. This behaves the same as if we had used componentDidMount()
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
     </>
-  );
-};
+  )
+}
 
-export default BubblePage;
+export default BubblePage
